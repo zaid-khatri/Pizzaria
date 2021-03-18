24f6597e2052f3dcaaf38e80c659213846ead886
zaid-khatri <76276783+zaid-khatri@users.noreply.github.com>
2021-03-18 12:05:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+menu_list_array = ["chicken tandoori pizza",
+                   "veg supereme pizza",
+                   "paneer tickka pizza",
+                   "deluxe veggie pizza",
+                "veg extravaganza pizza"];
+function getmenu(){
+var htmldata;
+htmldata="<ol class='menulist'>"
+menu_list_array.sort();
+for(var i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata+'<li>'+menu_list_array[i] + '</li>'
+}
+htmldata=htmldata+"</ol>"
+document.getElementById("display_menu").innerHTML = htmldata;
+};
+function add_item{
+var htmldata
+ var item=document.getElementById("add_item").value
+ menu_list_array.push(item);
+ menu_list_array.sort();
+ htmldata="<section class='cards'>"
+ for(var i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata+'<div class="cards">'+'<img src="images/pizzaImg.png"/>'+menu_list_array[i]+'</div>'
+ }
+ htmldata=htmldata+"</section>"
+ document.getElementById("display_addedmenu").innerHTML = htmldata;
+}
